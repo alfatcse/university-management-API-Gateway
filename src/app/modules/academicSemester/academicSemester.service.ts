f3cf53a7bb1f6f65e57c946201005b50e2ed2acd
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-09-17 19:43:21 +0200

    academic semester route added

---
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -0,0 +1,14 @@
+import { CoreService as HttpService } from '../../../shared/axios';
+
+const insertIntoDB = async (req) => {
+  console.log(req);
+  const response = await HttpService.post('/academic-semesters', req.body, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
+export const AcademicSemesterService = {
+  insertIntoDB
+};
