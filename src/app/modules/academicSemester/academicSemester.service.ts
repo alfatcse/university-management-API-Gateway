59143e05d2aa9647f58a1c8998763146c8f38373
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-09-17 20:35:26 +0200

    Req error fixed

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,8 +1,10 @@
+import { Request } from 'express';
 import { CoreService as HttpService } from '../../../shared/axios';
+import { IGenericResponse } from '../../../interfaces/common';
 
-const insertIntoDB = async (req) => {
-  console.log(req);
-  const response = await HttpService.post('/academic-semesters', req.body, {
+const insertIntoDB = async (req: Request): Promise<IGenericResponse> => {
+  console.log(req.body);
+  const response: IGenericResponse = await HttpService.post('/academic-semesters', req.body, {
     headers: {
       Authorization: req.headers.authorization
     }
