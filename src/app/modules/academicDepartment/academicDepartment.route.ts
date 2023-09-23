b117423f14e3bf691f2542cac438b08dbe954030
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-09-23 12:03:02 +0200

    academic Department update

--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -5,22 +5,26 @@ import { ENUM_USER_ROLE } from '../../../enums/user';
 import { AcademicDepartmentValidation } from './academicDepartment.validation';
 import { AcademicDepartmentController } from './academicDepartment.controller';
 const router = express.Router();
+router.get('/', AcademicDepartmentController.getAllFromDB);
+router.get('/:id', AcademicDepartmentController.getByIdFromDB);
 router.post(
   '/',
   validateRequest(AcademicDepartmentValidation.create),
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicDepartmentController.insertIntoDB
 );
-// router.get('/', AcademicSemesterController.getAllFromDB);
-// router.patch(
-//   '/:id',
-//   validateRequest(AcademicSemesterValidation.update),
-//   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-//   AcademicSemesterController.updateOneIntoDB
-// );
-// router.delete(
-//   '/:id',
-//   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-//   AcademicSemesterController.deleteByIdFromDB
-// );
+
+router.patch(
+  '/:id',
+  validateRequest(AcademicDepartmentValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicDepartmentController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicDepartmentController.deleteByIdFromDB
+);
+
 export const academicDepartmentRoutes = router;
--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -10,41 +10,54 @@ const insertIntoDB = async (req: Request): Promise<IGenericResponse> => {
   });
   return response;
 };
-// const getAllFromDB = async (req: Request): Promise<IGenericResponse> => {
-//   const response: IGenericResponse = await HttpService.get('/academic-semesters', {
-//     params: req.query,
-//     headers: {
-//       Authorization: req.headers.authorization
-//     }
-//   });
-//   return response;
-// };
-// const updateOneIntoDB = async (req: Request): Promise<IGenericResponse> => {
-//   const response: IGenericResponse = await HttpService.patch(
-//     `/academic-semesters/${req.params.id}`,
-//     req.body,
-//     {
-//       headers: {
-//         Authorization: req.headers.authorization
-//       }
-//     }
-//   );
-//   return response;
-// };
-// const deleteByIdFromDB = async (req: Request): Promise<IGenericResponse> => {
-//   const response: IGenericResponse = await HttpService.delete(
-//     `/academic-semesters/${req.params.id}`,
-//     {
-//       headers: {
-//         Authorization: req.headers.authorization
-//       }
-//     }
-//   );
-//   return response;
-// };
+const getAllFromDB = async (req: Request): Promise<IGenericResponse> => {
+  const response: IGenericResponse = await HttpService.get('/academic-departments', {
+    params: req.query,
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
+
+const getByIdFromDB = async (req: Request): Promise<IGenericResponse> => {
+  const { id } = req.params;
+  const response: IGenericResponse = await HttpService.get(`/academic-departments/${id}`, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
+
+const updateOneInDB = async (req: Request): Promise<IGenericResponse> => {
+  const { id } = req.params;
+  console.log(id);
+  const response: IGenericResponse = await HttpService.patch(
+    `/academic-departments/${id}`,
+    req.body,
+    {
+      headers: {
+        Authorization: req.headers.authorization
+      }
+    }
+  );
+  return response;
+};
+
+const deleteByIdFromDB = async (req: Request): Promise<IGenericResponse> => {
+  const { id } = req.params;
+  const response: IGenericResponse = await HttpService.delete(`/academic-departments/${id}`, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
 export const AcademicDepartmentService = {
-  insertIntoDB
-  //   getAllFromDB,
-  //   updateOneIntoDB,
-  //   deleteByIdFromDB
+  insertIntoDB,
+  getAllFromDB,
+  updateOneInDB,
+  deleteByIdFromDB,
+  getByIdFromDB
 };
